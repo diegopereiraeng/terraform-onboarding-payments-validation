a848faad1f01de7c08d17c677b15f71f6f7f2361
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2022-12-21 23:47:30 -0300

    dns record set

--- banking-demo-dns/terraform.tf
+++ banking-demo-dns/terraform.tf
@@ -1,27 +1,27 @@
 
 terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "3.5.0"
+    required_providers {
+        google = {
+        source = "hashicorp/google"
+        version = "3.5.0"
+        }
     }
-  }
 }
 
 // Configure the Google Cloud provider
 provider "google" {
-// credentials = "${file("${var.gcp_sa}")}" //
+ //credentials = "${file("${var.gcp_sa}")}" //
  project     = var.project //
- //region      = var.region //"us-central1-a"
+ region      = var.region //"us-central1-a"
 }
 
 resource "google_dns_record_set" "dns_record" {
-  name         = "${trim(var.gitUser, "\"")}-payments-validation.harness-demo.site."
-  managed_zone = "harness-demo"
-  type         = "A"
-  ttl          = 300
-  project     = var.project
-  rrdatas = [var.ipAddress]
+    name         = "${trim(var.gitUser, "\"")}-payments-validation.harness-demo.site."
+    managed_zone = "harness-demo"
+    type         = "A"
+    ttl          = 300
+    project     = var.project
+    rrdatas = [var.ipAddress]
 }
 
 # resource "google_dns_managed_zone" "prod" {
